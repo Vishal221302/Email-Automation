857ddb2237e5e82db7ad0971ed1a97c5f8702318
Vishal <vishalpatel221302@gmail.com>
2026-07-18 01:17:48 +0530

    update code

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { logout } from '../redux/slices/authSlice';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL:'https://email-automation-backend-dl1c.onrender.com/api' || import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
